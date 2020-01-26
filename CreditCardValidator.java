859fb888d823f59a1d9703eb5987051fefae8f72
Rahul B U <rahulbu83@gmail.com>
2020-01-26 14:13:53 +0530

    added credit_card_validator module

--- CreditCardValidator.java
+++ CreditCardValidator.java
@@ -5,22 +5,48 @@ class CreditCardValidator{
     public CreditCardValidator(long number){
         this.cardNumber = number;
     }
-
+    private int getLength(){
+        return String.valueOf(this.cardNumber).length();
+    }
+    private int sumOfEvenPlaces(){
+        int sum = 0;
+        String num = String.valueOf(cardNumber);
+        for(int i=num.length()-2; i>=0 ;i-=2){
+            int digit = Integer.valueOf(num.charAt(i)+"");
+            digit = digit*2;
+            digit = (String.valueOf(digit).length() != 1) ? (digit -9):digit;
+            sum += digit;
+        }
+        return sum;
+    }
+    private int sumOfOddPlaces(){
+        int sum = 0;
+        String num = String.valueOf(cardNumber);
+        for(int i= num.length()-1;i>=0;i-=2){
+            int digit = Integer.valueOf(num.charAt(i)+"");
+            sum += digit;
+        }
+        return sum;
+    }
     public boolean validate(){
-        
+        int len = getLength();
+        if(len >= 13 && len <=16){
+            if(((sumOfEvenPlaces() + sumOfOddPlaces()))%10 !=0 ){
+                return false;
+            }
+        }
+        else return false;
         return true;
     }
 
     public static void main(String[] args) {
         
         CreditCardValidator card ;
-        long cardNumbers[] = {1234567891234L, 4567890123456L, 89076543212345L};
+        long cardNumbers[] = {79927398713L, 4388576018402626L, 89076543212345L, 5196081888500645L, 4388576018402626L};
 
         for(long num : cardNumbers){
             card = new CreditCardValidator(num);
             System.out.println(num+" : "+card.validate());
         }
-
-        System.out.println();
     }
 }
